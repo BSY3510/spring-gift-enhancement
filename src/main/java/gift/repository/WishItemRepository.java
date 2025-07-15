9a25e8b37459caea975c6bf0c957324c0438cf24
BSY3510 <baeksy3510@gmail.com>
2025-07-16 02:55:26 +0900

    refactor(repo): remove findByMember in WishItemRepository

--- src/main/java/gift/repository/WishItemRepository.java
+++ src/main/java/gift/repository/WishItemRepository.java
@@ -7,6 +7,5 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface WishItemRepository extends JpaRepository<WishItem, Long> {
-    List<WishItem> findByMember(Member member);
     void deleteByIdAndMemberId(Long id, Long memberId);
 }
