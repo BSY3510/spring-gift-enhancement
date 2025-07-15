2fbd038a06d9327ce79f5cd5f3194d4e9d7eef1f
BSY3510 <baeksy3510@gmail.com>
2025-07-15 17:19:18 +0900

    test(config): update test configurations

--- src/test/resources/db/data.sql
+++ src/test/resources/db/data.sql
@@ -1,5 +1,2 @@
-insert into members (id, email, password, role) values (default, 'test@test.com', 'cGFzc3dvcmQxMjM=', 'USER');
-insert into members (id, email, password, role) values (123, 'remove@test.com', 'cGFzc3dvcmQxMjM=', 'USER');
-insert into products (id, name, price, image_url) values (default, 'Test Product', 1000, 'http://test.com');
-insert into products (id, name, price, image_url) values (123, 'Test Product', 1000, 'http://test.com');
-insert into wishItems (id, productId, quantity, memberId) values (default, 1, 2, 1);
+INSERT INTO member (id, email, password, role) VALUES (DEFAULT, 'test@test.com', 'cGFzc3dvcmQxMjM=', 'USER');
+INSERT INTO product (id, name, price, image_url) VALUES (DEFAULT, 'Test Product', 1000, 'http://test.com');
