99f47f6c082b1cdeaa08dec0e7f7dc6d3cc84aa9
BSY3510 <baeksy3510@gmail.com>
2025-07-15 17:10:44 +0900

    feat(entity): enhance WishItem entity with JPA

--- src/main/java/gift/entity/WishItem.java
+++ src/main/java/gift/entity/WishItem.java
@@ -1,14 +1,41 @@
 package gift.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "wish")
 public class WishItem {
 
-    private final Long id;
-    private final Product product;
-    private final Integer quantity;
-    private final Member member;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    private Integer quantity;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    protected WishItem() {}
 
     public WishItem(Long id, Product product, Integer quantity, Member member) {
         this.id = id;
+        this.product = product;
+        this.quantity = quantity;
+        this.member = member;
+    }
+
+    public WishItem(Product product, Integer quantity, Member member) {
         this.product = product;
         this.quantity = quantity;
         this.member = member;
